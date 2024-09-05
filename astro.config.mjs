d961126baa50061fb86569ef188e4d1e8541eedf
yossrimelki <yossrimelki@gmail.com>
2024-09-05 13:48:52 +0100

    fix
    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
@@ -9,30 +8,17 @@ import remarkCollapse from 'remark-collapse';
 import remarkToc from 'remark-toc';
 import config from './src/config/config.json';
 
-// https://astro.build/config
 export default defineConfig({
   site: config.site.base_url ? config.site.base_url : 'http://examplesite.com',
   base: config.site.base_path ? config.site.base_path : '/',
   trailingSlash: config.site.trailing_slash ? 'always' : 'never',
   output: 'hybrid',
-  image: {
-    service: squooshImageService(), // Ensure this function is defined or imported
-    api: {
-      routes: [
-        {
-          path: '/src/pages/api/send-mail.js',
-          method: 'POST',
-          handler: '/src/pages/api/send-mail.js',
-        },
-      ],
-    },
-  },
   integrations: [
     react(),
     sitemap(),
     tailwind({
       config: {
-        safelist: [], // Update or remove the safelist
+        safelist: [],
         applyBaseStyles: false,
       },
     }),
@@ -52,7 +38,7 @@ export default defineConfig({
       ],
     }),
     mdx(),
-    vercel(), // Ensure this is included
+    // Remove vercel() if it's causing issues
   ],
   markdown: {
     remarkPlugins: [
