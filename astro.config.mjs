66e6144ed39032cdc4aa3e4cbb3a8ebf827559a0
yossrimelki <yossrimelki@gmail.com>
2024-09-05 14:46:28 +0100

    fixxxxxx
    fixxxxx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,37 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel'; // Make sure this package is correctly installed
-import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import AutoImport from 'astro-auto-import';
-import remarkCollapse from 'remark-collapse';
-import remarkToc from 'remark-toc';
-import config from './src/config/config.json';
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import AutoImport from "astro-auto-import";
+import { defineConfig,squooshImageService } from "astro/config";
+import remarkCollapse from "remark-collapse";
+import remarkToc from "remark-toc";
+import config from "./src/config/config.json";
 
+// https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url || 'http://examplesite.com',
-  base: config.site.base_path || '/',
-  trailingSlash: config.site.trailing_slash ? 'always' : 'never',
+  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  base: config.site.base_path ? config.site.base_path : "/",
+  trailingSlash: config.site.trailing_slash ? "always" : "never",
   output: 'hybrid',
+  image: {
+    service: squooshImageService(),  // Switch to Sharp image service
+    api: {
+      routes: [
+        {
+          path: '/src/pages//api/send-mail.js',
+          method: 'POST',
+          handler: '/src/pages/api/send-mail.js',// Update this to point to your handler function
+        },
+      ],
+    },
+  },
   integrations: [
     react(),
     sitemap(),
     tailwind({
       config: {
-        safelist: [],
+        safelist: [], // Update or remove the safelist
         applyBaseStyles: false,
       },
     }),
@@ -39,7 +51,6 @@ export default defineConfig({
       ],
     }),
     mdx(),
-    vercel(), // Ensure this is correctly integrated
   ],
   markdown: {
     remarkPlugins: [
